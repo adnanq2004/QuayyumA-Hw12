1213ece9a0835ed31a5ea0a7173df6475146a835
adnanq2004 <aquayyum20@stuy.edu>
2021-11-03 18:50:14 -0400

    i think i'm done

--- hw12.c
+++ hw12.c
@@ -3,76 +3,90 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <string.h>
 
 int main(int argc, char *argv[]) {
 
   struct dirent* point;
   struct stat* info;
   int total = 0;
-  char * dir = NULL;
 
-  if (argc == 2) {
-    dir = argv[1];
-    // strcpy(dir,argv[1]);
-  }
-  else if (argc > 2) {
-    printf("too many inputs, please provide fewer inputs");
+  char dir[100];
+  if (argc  > 1) {
+    strcpy(dir, argv[1]);
   }
   else {
-    while (!dir) {
-      // scanf("please provide a valid directory: %s\n", dir);
-      // if (!dir) {
-      //   int errnum = errno;
-      //   // printf("there was an error:\n%s\n", perror(errno));
-      //   perror("there was an error");
-      // }
-      // dir = "./";
-      scanf("%s\n", dir);
-      // if (!dir) {
-        // printf("there was an error: %s\n", strerror(errno));
-        // int errnum = errno;
-        // perror(errnum);
-        // printf("there was an error:\n\n%s\n\n", errlang);
-      // }
+    printf("please provide a proper directory\n");
+    scanf("%s",dir);
+    if (opendir(dir) < 0) {
+      printf("Error %d: %s\n", errno, strerror(errno));
+      return 0;
     }
   }
+
+  // char * dir = NULL;
+  // if (argc == 2) {
+  //   dir = argv[1];
+  //   // strcpy(dir,argv[1]);
+  // }
+  // else if (argc > 2) {
+  //   printf("too many inputs, please provide fewer inputs");
+  // }
+  // else {
+  //   while (!dir) {
+  //     // scanf("please provide a valid directory: %s\n", dir);
+  //     // if (!dir) {
+  //     //   int errnum = errno;
+  //     //   // printf("there was an error:\n%s\n", perror(errno));
+  //     //   perror("there was an error");
+  //     // }
+  //     // dir = "./";
+  //     scanf("%s\n", dir);
+  //     // if (!dir) {
+  //       // printf("there was an error: %s\n", strerror(errno));
+  //       // int errnum = errno;
+  //       // perror(errnum);
+  //       // printf("there was an error:\n\n%s\n\n", errlang);
+  //     // }
+  //   }
+  // }
 
   printf("statistics for directory: %s\n\n", dir);
 
   DIR * direct1 = opendir(dir);
-  printf("directories\n\n");
+  printf("directories:\n");
   point = readdir(direct1);
   while (point && point->d_type == DT_DIR) {
     info = malloc(sizeof(struct stat));
     stat(point->d_name, info);
     total += info->st_size;
-    printf("%s\n", point->d_name);
+    printf("           %s\n", point->d_name);
     point = readdir(direct1);
   }
   closedir(direct1);
   printf("\n");
 
   DIR * direct2 = opendir(dir);
-  printf("regular files\n");
+  printf("regular files:\n");
   point = readdir(direct2);
   while (point && point->d_type == DT_REG) {
     info = malloc(sizeof(struct stat));
     stat(point->d_name, info);
     total += info->st_size;
-    printf("%s\n", point->d_name);
+    printf("           %s\n", point->d_name);
     point = readdir(direct2);
   }
   closedir(direct2);
   printf("\n");
 
   DIR * direct3 = opendir(dir);
-  printf("other files files\n");
+  printf("other files files:\n");
   point = readdir(direct3);
   while (point && point->d_type != DT_REG && point->d_type != DT_DIR) {
     info = malloc(sizeof(struct stat));
     stat(point->d_name, info);
     total += info->st_size;
-    printf("%s\n", point->d_name);
+    printf("           %s\n", point->d_name);
     point = readdir(direct3);
   }
   closedir(direct3);
