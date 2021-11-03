e95cc210ce8b28c75a4c067c5fb1992ecb7e104b
adnanq2004 <aquayyum20@stuy.edu>
2021-11-03 18:29:24 -0400

    the scanf part is still not working

--- hw12.c
+++ hw12.c
@@ -2,15 +2,44 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <errno.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
   struct dirent* point;
   struct stat* info;
   int total = 0;
-  printf("statistics for directory: ./\n\n");
+  char * dir = NULL;
 
-  DIR * direct1 = opendir("./");
+  if (argc == 2) {
+    dir = argv[1];
+    // strcpy(dir,argv[1]);
+  }
+  else if (argc > 2) {
+    printf("too many inputs, please provide fewer inputs");
+  }
+  else {
+    while (!dir) {
+      // scanf("please provide a valid directory: %s\n", dir);
+      // if (!dir) {
+      //   int errnum = errno;
+      //   // printf("there was an error:\n%s\n", perror(errno));
+      //   perror("there was an error");
+      // }
+      // dir = "./";
+      scanf("%s\n", dir);
+      // if (!dir) {
+        // printf("there was an error: %s\n", strerror(errno));
+        // int errnum = errno;
+        // perror(errnum);
+        // printf("there was an error:\n\n%s\n\n", errlang);
+      // }
+    }
+  }
+
+  printf("statistics for directory: %s\n\n", dir);
+
+  DIR * direct1 = opendir(dir);
   printf("directories\n\n");
   point = readdir(direct1);
   while (point && point->d_type == DT_DIR) {
@@ -23,7 +52,7 @@ int main() {
   closedir(direct1);
   printf("\n");
 
-  DIR * direct2 = opendir("./");
+  DIR * direct2 = opendir(dir);
   printf("regular files\n");
   point = readdir(direct2);
   while (point && point->d_type == DT_REG) {
@@ -36,7 +65,7 @@ int main() {
   closedir(direct2);
   printf("\n");
 
-  DIR * direct3 = opendir("./");
+  DIR * direct3 = opendir(dir);
   printf("other files files\n");
   point = readdir(direct3);
   while (point && point->d_type != DT_REG && point->d_type != DT_DIR) {
