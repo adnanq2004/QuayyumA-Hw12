5494f0d2109947852c86112bdba04d6069ede4c9
adnanq2004 <aquayyum20@stuy.edu>
2021-11-03 17:36:05 -0400

    made the code match example more

--- hw12.c
+++ hw12.c
@@ -5,29 +5,52 @@
 
 int main() {
 
-  DIR * direct = opendir("./");
   struct dirent* point;
   struct stat* info;
-  point = readdir(direct);
   int total = 0;
-  printf("statistics for directory: .\n");
-  while (point) {
-    printf("%s ", point->d_name);
+  printf("statistics for directory: ./\n\n");
+
+  DIR * direct1 = opendir("./");
+  printf("directories\n\n");
+  point = readdir(direct1);
+  while (point && point->d_type == DT_DIR) {
     info = malloc(sizeof(struct stat));
     stat(point->d_name, info);
-    if (point->d_type == DT_REG) {
-      total += info->st_size;
-      printf("is regular file\n\n");
-    }
-    else if (point->d_type == DT_DIR) {
-      printf("is directory\n\n");
-    }
-    else {
-      printf("is other file\n\n");
-    }
-    free(info);
-    point = readdir(direct);
+    total += info->st_size;
+    printf("%s\n", point->d_name);
+    point = readdir(direct1);
   }
+  closedir(direct1);
+  printf("\n");
+
+  DIR * direct2 = opendir("./");
+  printf("regular files\n");
+  point = readdir(direct2);
+  while (point && point->d_type == DT_REG) {
+    info = malloc(sizeof(struct stat));
+    stat(point->d_name, info);
+    total += info->st_size;
+    printf("%s\n", point->d_name);
+    point = readdir(direct2);
+  }
+  closedir(direct2);
+  printf("\n");
+
+  DIR * direct3 = opendir("./");
+  printf("other files files\n");
+  point = readdir(direct3);
+  while (point && point->d_type != DT_REG && point->d_type != DT_DIR) {
+    info = malloc(sizeof(struct stat));
+    stat(point->d_name, info);
+    total += info->st_size;
+    printf("%s\n", point->d_name);
+    point = readdir(direct3);
+  }
+  closedir(direct3);
+  printf("\n");
+
   printf("total size: %d\n", total);
+
+  return 0;
 
 }
